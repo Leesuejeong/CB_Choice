01d48e47d552051a5589a81614399b000650271f
Sujeong Lee <olivia7342@naver.com>
2019-05-23 11:41:18 +0900

    #2 콤보박스 항목 추가, 선택, 버튼 클릭 코드 구현

--- CB_Choice/Form1.Designer.cs
+++ CB_Choice/Form1.Designer.cs
@@ -32,6 +32,7 @@
             this.cbList = new System.Windows.Forms.ComboBox();
             this.txtList = new System.Windows.Forms.TextBox();
             this.btnAdd = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblResult
@@ -48,10 +49,11 @@
             // 
             this.cbList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbList.FormattingEnabled = true;
-            this.cbList.Location = new System.Drawing.Point(90, 40);
+            this.cbList.Location = new System.Drawing.Point(90, 62);
             this.cbList.Name = "cbList";
             this.cbList.Size = new System.Drawing.Size(316, 20);
             this.cbList.TabIndex = 2;
+            this.cbList.SelectedIndexChanged += new System.EventHandler(this.cbList_SelectedIndexChanged);
             // 
             // txtList
             // 
@@ -68,12 +70,25 @@
             this.btnAdd.TabIndex = 4;
             this.btnAdd.Text = "추가";
             this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            this.btnAdd.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.btnAdd_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(194, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 18);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "김가네 메뉴";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(499, 227);
+            this.ClientSize = new System.Drawing.Size(499, 204);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.txtList);
             this.Controls.Add(this.cbList);
@@ -82,6 +97,7 @@
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "메뉴 선택 추가";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -93,6 +109,7 @@
         private System.Windows.Forms.ComboBox cbList;
         private System.Windows.Forms.TextBox txtList;
         private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- CB_Choice/Form1.cs
+++ CB_Choice/Form1.cs
@@ -11,9 +11,68 @@ namespace CB_Choice
 {
     public partial class Form1 : Form
     {
+        //C#은 기본 접근제어자가 private
+        string[] SList = new string[]
+        {
+            "돈까스김밥", "스팸김치볶음밥", "라볶이", "쫄면", "참치주먹밥", "치즈라면"
+        };
+        string OrgStr = "";
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for(int i = 0; i < SList.Length; i++)
+            {
+                this.cbList.Items.Add(this.SList[i]);
+            }
+            this.OrgStr = this.lblResult.Text;
+            if (this.SList.Length > 0)
+            {
+                this.cbList.SelectedIndex = 0;
+            }
+            
+        }
+
+        private void cbList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.lblResult.Text = this.OrgStr + this.cbList.Text;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Checkinput();
+        }
+
+        private void btnAdd_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                Checkinput();
+            }
+        }
+
+        private bool Checkinput()
+        {
+
+            if (this.txtList.Text == "")
+            {
+                MessageBox.Show("추가할 항목을 입력해주세요!", "알림", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                this.txtList.Focus();
+                return false;
+            }
+            else
+            {
+                this.cbList.Items.Add(this.txtList.Text);
+                this.txtList.Text = "";
+                this.cbList.SelectedIndex = this.cbList.Items.Count - 1;
+                this.txtList.Focus();
+                return true;
+            }
+        }
+
+        
     }
 }
